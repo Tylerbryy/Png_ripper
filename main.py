0e50f211f4dca81c6e4b07cb6763504fd9dc8150
tylerbry <tylergibbs049@gmail.com>
2023-09-10 13:57:18 -0500

    added output to be json too

--- main.py
+++ main.py
@@ -44,7 +44,9 @@ def consolidate_prompts(raw_text):
     return '\n'.join(prompts)
 
 
-def extract_prompts_from_folder():
+import json
+
+def extract_prompts_from_folder(output_format='txt'):
     """Extract prompts from images in the specified folder and save to an output file."""
     root = Tk()
     root.withdraw()  # Hide the main window.
@@ -52,42 +54,43 @@ def extract_prompts_from_folder():
     if not folder_path:
         print("No folder selected. Exiting...")
         return
-    output_file = "prompts_" + datetime.now().strftime("%Y%m%d") + ".txt"
     
-    with open(output_file, 'w') as f:
-        # Get the list of files
-        files = os.listdir(folder_path)
-        # Initialize the progress bar
-        pbar = tqdm(total=len(files))
-        for filename in files:
-            if filename.endswith(".jpg"):
-                file_path = os.path.join(folder_path, filename)
-                
-                reader = ImageDataReader(file_path)
-                prompt = reader.positive
-                
-                # Error handling for empty jpeg
-                if not prompt:
-                    os.system('cls' if os.name == 'nt' else 'clear')
-                    print(Fore.RED + f"{filename} is a no parameters " + Fore.LIGHTGREEN_EX + "(skipped)" + Style.RESET_ALL)
-                else:
-                    f.write(prompt + ' ##' + '\n')  # Added ' ##' to the prompt and an extra '\n' to insert a line between prompts
-            # Update the progress bar
-            pbar.update(1)
-        # Close the progress bar
-        pbar.close()
-        
-    # After writing all the prompts to the output file:
-    with open(output_file, 'r') as f:
-        raw_output = f.read()
+    # Adjust the output filename based on the chosen format
+    output_file = "prompts_" + datetime.now().strftime("%Y%m%d") + "." + output_format
     
-    formatted_output = consolidate_prompts(raw_output)
+    extracted_prompts = []
     
-    with open(output_file, 'w') as f:
-        f.write(formatted_output)
-
-
+    # Get the list of files
+    files = os.listdir(folder_path)
+    # Initialize the progress bar
+    pbar = tqdm(total=len(files))
+    for filename in files:
+        if filename.endswith(".jpg"):
+            file_path = os.path.join(folder_path, filename)
+            
+            reader = ImageDataReader(file_path)
+            prompt = reader.positive
+            
+            # Error handling for empty jpeg
+            if not prompt:
+                os.system('cls' if os.name == 'nt' else 'clear')
+                print(Fore.RED + f"{filename} is a no parameters " + Fore.LIGHTGREEN_EX + "(skipped)" + Style.RESET_ALL)
+            else:
+                extracted_prompts.append(prompt)
+        # Update the progress bar
+        pbar.update(1)
+    # Close the progress bar
+    pbar.close()
+    
+    # Save the extracted prompts based on the chosen format
+    if output_format == 'txt':
+        with open(output_file, 'w') as f:
+            f.write(consolidate_prompts('\n'.join([prompt + ' ##' for prompt in extracted_prompts])))
+    elif output_format == 'json':
+        with open(output_file, 'w') as f:
+            json.dump(extracted_prompts, f, indent=4)
 
 if __name__ == "__main__":
     print(Fore.YELLOW + "Welcome to the PNG Ripper. Please select a folder to extract prompts from." + Style.RESET_ALL)
-    extract_prompts_from_folder()
+    format_choice = input("Choose an output format (txt/json) [default: txt]: ").strip().lower() or 'txt'
+    extract_prompts_from_folder(format_choice)
